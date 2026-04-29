bf15ed4154d106265a494c6e04803d35bd6be9da
Tristen <tahale1@csuchico.edu>
2026-04-29 11:37:37 -0700

    initial tests

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -5,6 +5,8 @@
 #include <gtest/gtest.h>
 #include "Guesser.h"
 
+#include <string>
+
 class GuesserTest : public ::testing::Test
 {
 	protected:
@@ -14,9 +16,91 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+// distance tests
+TEST(GuesserTest, distance__basic_1)
 {
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+  Guesser object("AAA");
+  ASSERT_EQ( 1, object.distance("AAB") );
+}
+TEST(GuesserTest, distance__basic_2)
+{
+  Guesser object("AAA");
+  ASSERT_EQ( 2, object.distance("ABB") );
+}
+TEST(GuesserTest, distance__basic_3)
+{
+  Guesser object("AAA");
+  ASSERT_EQ( 3, object.distance("BBB") );
+}
+TEST(GuesserTest, distance__substr_1)
+{
+	// create strings equal to 32
+	std::string base, guess; 
+	for(int i = 0; i < 32; i++){
+		base += 'A';
+		guess += 'A';
+	}
+
+  Guesser object(base);
+  ASSERT_EQ( 0, object.distance(guess) );
+}
+TEST(GuesserTest, distance__substr_2)
+{
+	// create strings greater than 32
+	std::string base, guess; 
+	for(int i = 0; i < 33; i++){
+		base += 'A';
+		guess += 'A';
+	}
+
+  Guesser object(base);
+  ASSERT_EQ( 1, object.distance(guess) );
+}
+TEST(GuesserTest, distance__substr_3)
+{
+	// create strings greater than 32
+	std::string base, guess; 
+	for(int i = 0; i < 33; i++){
+		base += 'A';
+		guess += 'B';
+	}
+
+  Guesser object(base);
+  ASSERT_EQ( 32, object.distance(guess) );
+}
+
+// match tests
+TEST(GuesserTest, match__basic_true)
+{
+  Guesser object("AAA");
+  ASSERT_TRUE( object.match("AAA") );
+}
+TEST(GuesserTest, match__basic_false)
+{
+  Guesser object("AAA");
+  ASSERT_FALSE( object.match("BBB") );
+}
+TEST(GuesserTest, distance__substr_basic_true)
+{
+	// create strings greater than 32
+	std::string base, guess; 
+	for(int i = 0; i < 32; i++){
+		base += 'A';
+		guess += 'A';
+	}
+
+  Guesser object(base);
+  ASSERT_TRUE( object.match(guess) );
+}
+TEST(GuesserTest, match__substr_basic_false)
+{
+	// create strings greater than 32
+	std::string base, guess; 
+	for(int i = 0; i < 32; i++){
+		base += 'A';
+		guess += 'B';
+	}
+
+  Guesser object(base);
+  ASSERT_FALSE( object.match(guess) );
 }
